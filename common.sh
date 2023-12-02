550806a1800c83a0d991563a18bbfad97ee9ac48
mattmcw <matt@sixteenmillimeter.com>
2023-12-02 15:43:12 -0500

    Add function for checking if a test string is valid

--- common.sh
+++ common.sh
@@ -7,3 +7,12 @@ function validateNumber () {
 		echo "Test failed $1 != $2"
 	fi
 }
+
+
+function validateString () {
+	if [[ "$1" -eq "$2" ]]; then
+		echo "Test passed \"$1\" != \"$2\""
+	else
+		echo "Test failed \"$1\" != \"$2\""
+	fi
+}

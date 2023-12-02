2bdb1f41fbbc0939f260c5aac7001c2d340c429a
mattmcw <matt@sixteenmillimeter.com>
2023-12-02 15:43:36 -0500

    Scripts and files in place for tonight

---
+++ 03/02.cpp
@@ -0,0 +1,12 @@
+#include "../aoc.h"
+
+AOC aoc;
+
+int main() {
+   string s;
+   int64_t answer = 0;
+   while( getline( cin, s ) ) {
+      
+   }
+   cout << answer << endl;
+}
---
+++ 03/test02.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+source ../common.sh
+
+set -e
+
+g++ -std=c++11 -o 02 02.cpp
+
+ANSWER=0
+#cat example02.txt | ./02
+TEST=$(cat example01.txt | ./02)
+
+validateNumber $TEST $ANSWER

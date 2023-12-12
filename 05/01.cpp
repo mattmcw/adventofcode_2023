06b6b8fe7a8f3590060ac3391ea064401166cfbf
mmcwilliams <hi@mmcwilliams.com>
2023-12-12 00:26:24 -0500

    C++ solve for 05a

--- 05/01.cpp
+++ 05/01.cpp
@@ -1,18 +1,40 @@
 #include "../aoc.h"
 
 AOC aoc;
+char* endptr;
 
-vector<int64_t> splitMap (string map, vector<int64_t> input) {
+vector<int64_t> splitMap (string map, vector<int64_t> input, vector<int64_t> compare) {
    vector<string> parts = aoc.split(aoc.trim(map), aoc.SPACE);
-   int64_t startd = stoll(parts[0]);
-   int64_t starts = stoll(parts[1]);
-   int64_t range = stoll(parts[2]);
+   int64_t startd = strtol(parts[0].c_str(), &endptr, 10);
+   int64_t starts = strtol(parts[1].c_str(), &endptr, 10);
+   int64_t range = strtol(parts[2].c_str(), &endptr, 10);
    int64_t dest = startd;
    int64_t src = starts;
-   for (int64_t i = 0; i < range; i++) {
-      
+   int64_t diff;
+
+   cout << map << endl;
+   cout << startd << "-" << (startd + range) << " ";
+   cout << starts << "-" << (starts + range) << endl;
+
+   for (int i = 0; i < input.size(); i++) {
+      if (input[i] >= starts && input[i] <= starts + range) {
+         diff = input[i] - starts;
+         compare[i] = startd + diff;
+         cout << "* " << input[i] << "->" << compare[i] << endl;
+      }
    }
-   return input;
+   
+   return compare;
+}
+
+void display (vector<int64_t> &line) {
+   for (int i = 0; i < line.size(); i++) {
+      cout << line[i];
+      if (i < line.size() - 1) {
+         cout << ",";
+      }
+   }
+   cout << endl;
 }
 
 int main() {
@@ -27,11 +49,18 @@ int main() {
    string source;
    string tmp;
    vector<string> seedstr;
+   int64_t lowest = INT64_MAX;
 
-   vector<vector<int64_t>> data;
-
-   vector<int64_t> tmpsrc;
-   vector<int64_t> tmpdest;
+   vector<vector<int64_t>> data = {
+      {},
+      {},
+      {},
+      {},
+      {},
+      {},
+      {},
+      {}
+   };
 
    vector<string> keys = {
       "seeds",
@@ -49,10 +78,14 @@ int main() {
          parts = aoc.split(s, aoc.COLON);
          seedstr = aoc.split(aoc.trim(parts[1]), aoc.SPACE);
          for (string x : seedstr) {
-            data[0].push_back(stoll(x));
+            data[0].push_back(strtol(x.c_str(), &endptr, 10));
          }
-         for (int i = 1; i < keys.size(); i++) {
-            data[i].reserve(data[0].size());
+         display(data[0]);
+         for (int i = 1; i < data.size(); i++) {
+            for (int x = 0; x < data[0].size(); x++) {
+               data[i].push_back((int64_t) -1);
+            }
+            display(data[i]);
          }
       } else if (s.find("-") != string::npos) {
          parts = aoc.split(s, aoc.SPACE);
@@ -60,20 +93,24 @@ int main() {
          destination = map[0];
          source = map[2];
          cout << destination << " " << source << endl;
-      } else if (!data[0].empty()) {
-         tmpdest = splitMap(aoc.trim(s), 0);
-         tmpsrc = splitMap(aoc.trim(s), 1);
+      } else if (!data[0].empty() && aoc.trim(s) != "") {
          idx = aoc.getVecPosString(keys, source);
-         for (int i = 0; i < data[idx - 1].size(); i++) {
-            x = aoc.getVecPosInt(tmpsrc, data[idx - 1][i]);
-            if (x > -1) {
-               data[idx][i] = tmpdest[x];
+         data[idx] = splitMap(aoc.trim(s), data[idx - 1], data[idx]);
+         display(data[idx]);
+         for (int i = 0; i < data[idx].size(); i++) {
+            if (data[idx][i] == -1) {
+               data[idx][i] = data[idx - 1][i];
             }
-         }
-         for (int i = 0; i <  data[idx].size(); i++) {
-            cout << data[idx - 1][i] << " = " << data[idx][i] << endl;
          }
       }
    }
+
+   for (int64_t val : data[7]) {
+      if (val < lowest) {
+         lowest = val;
+      }
+   }
+   answer = lowest;
+
    cout << "ANSWER " << answer << endl;
 }

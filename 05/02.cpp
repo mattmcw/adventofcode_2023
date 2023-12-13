64b5a43627f26c314a500f936c83506be0bfb6d0
mattmcw <matt@sixteenmillimeter.com>
2023-12-12 22:23:17 -0500

    use uint64_t not int64_t (no negatives are used)

--- 05/02.cpp
+++ 05/02.cpp
@@ -3,21 +3,21 @@
 AOC aoc;
 char* endptr;
 
-vector<int64_t> splitMap (string map, vector<int64_t> &input, vector<int64_t> &compare) {
+vector<uint64_t> splitMap (string map, vector<uint64_t> &input, vector<uint64_t> &compare) {
    vector<string> parts = aoc.split(aoc.trim(map), aoc.SPACE);
-   int64_t startd = strtol(parts[0].c_str(), &endptr, 10);
-   int64_t starts = strtol(parts[1].c_str(), &endptr, 10);
-   int64_t range = strtol(parts[2].c_str(), &endptr, 10);
-   int64_t dest = startd;
-   int64_t src = starts;
-   int64_t diff;
+   uint64_t startd = strtol(parts[0].c_str(), &endptr, 10);
+   uint64_t starts = strtol(parts[1].c_str(), &endptr, 10);
+   uint64_t range = strtol(parts[2].c_str(), &endptr, 10);
+   uint64_t dest = startd;
+   uint64_t src = starts;
+   uint64_t diff;
 
    //cout << map << endl;
    //cout << startd << "-" << (startd + range) << " ";
    //cout << starts << "-" << (starts + range) << endl;
 
-   for (int64_t i = 0; i < input.size(); i++) {
-      if (input[i] >= starts && input[i] <= starts + range && compare[i] < (int64_t) 0) {
+   for (uint64_t i = 0; i < input.size(); i++) {
+      if (input[i] >= starts && input[i] <= starts + range && compare[i] < (uint64_t) 0) {
          diff = input[i] - starts;
          compare[i] = startd + diff;
          //cout << "* " << input[i] << "->" << compare[i] << endl;
@@ -27,16 +27,16 @@ vector<int64_t> splitMap (string map, vector<int64_t> &input, vector<int64_t> &c
    return compare;
 }
 
-vector<int64_t> fix (vector<int64_t> &input, vector<int64_t> &output) {
-   for (int64_t i = 0; i < input.size(); i++) {
-      if (output[i] == (int64_t) -1) {
+vector<uint64_t> fix (vector<uint64_t> &input, vector<uint64_t> &output) {
+   for (uint64_t i = 0; i < input.size(); i++) {
+      if (output[i] == (uint64_t) -1) {
          output[i] = input[i];
       }
    }
    return output;
 }
 
-void display (vector<int64_t> &line) {
+void display (vector<uint64_t> &line) {
    for (int i = 0; i < line.size(); i++) {
       cout << line[i];
       if (i < line.size() - 1) {
@@ -48,7 +48,7 @@ void display (vector<int64_t> &line) {
 
 int main() {
    string s;
-   int64_t answer = 0;
+   uint64_t answer = 0;
    int idx;
    int x;
    vector<string> parts;
@@ -58,11 +58,11 @@ int main() {
    string source;
    string tmp;
    vector<string> seedstr;
-   int64_t lowest = INT64_MAX;
-   int64_t start;
-   int64_t range;
+   uint64_t lowest = INT64_MAX;
+   uint64_t start;
+   uint64_t range;
 
-   vector<vector<int64_t>> data = {
+   vector<vector<uint64_t>> data = {
       {},
       {},
       {},
@@ -89,18 +89,18 @@ int main() {
       if (data[0].empty() && s.find("seeds:") != string::npos) {
          parts = aoc.split(s, aoc.COLON);
          seedstr = aoc.split(aoc.trim(parts[1]), aoc.SPACE);
-         for (int64_t i = 0; i < (int64_t) round(seedstr.size() / 2); i++) {
+         for (uint64_t i = 0; i < (uint64_t) round(seedstr.size() / 2); i++) {
             start = strtol(seedstr[i * 2].c_str(), &endptr, 10);
             range = strtol(seedstr[(i * 2) + 1].c_str(), &endptr, 10);
-            for (int64_t x = 0; x < range; x++) {
+            for (uint64_t x = 0; x < range; x++) {
                data[0].push_back(start + x);
             }
          }
          //display(data[0]);
          //cout << "SEEDS: " << data[0].size() << endl;
          for (int i = 1; i < data.size(); i++) {
-            for (int64_t x = 0; x < data[0].size(); x++) {
-               data[i].push_back((int64_t) -1);
+            for (uint64_t x = 0; x < data[0].size(); x++) {
+               data[i].push_back((uint64_t) -1);
             }
             //display(data[i]);
          }
@@ -115,7 +115,7 @@ int main() {
          }
          //cout << "----" << endl;
          //display(data[idx - 1]);
-         //cout << destination << " " << source << endl;
+         cout << destination << " " << source << endl;
       } else if (!data[0].empty() && aoc.trim(s) != "") {
          //cout << s << endl;
          data[idx] = splitMap(aoc.trim(s), data[idx - 1], data[idx]);
@@ -127,7 +127,7 @@ int main() {
    //cout << "----" << endl;
    //display(data[7]);
 
-   for (int64_t val : data[7]) {
+   for (uint64_t val : data[7]) {
       if (val < lowest) {
          lowest = val;
       }
